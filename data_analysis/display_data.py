f6abae5d62f8b93755d4023fdebeda7b3d96484c
Steve Novakov <stevenovakov@gmail.com>
2014-12-28 17:41:02 -0800

    testing git implementation on new system, added first plotting file

---
+++ data_analysis/display_data.py
@@ -0,0 +1,17 @@
+#
+# ENPH 479
+# University of British Columbia
+#
+# Creation Date:
+#			2014-12-28	
+#
+# Author(s):
+#			Steve Novakov
+#
+
+import os
+import matplotlib.pyplot as pt
+
+# this is the data root, specify power/temp/old data directory later
+data_dir = os.environ['DATA479']
+
