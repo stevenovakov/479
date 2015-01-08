10f40d3416ce6af59d7ca6568f83dd5a8adaf34c
Steve Novakov <stevenovakov@gmail.com>
2015-01-07 22:57:11 -0800

    added routine to manually find slopes around lock points for power data

--- data_analysis/display_data.py
+++ data_analysis/display_data.py
@@ -29,7 +29,15 @@ from savitzkygolay import SavitzkyGolay
 
 # this is the data root, DROPBOX/ENPH\ 479/rubidium
 # specify power/temp/old data directory later
-data_dir = os.environ['DATA479']
+
+data_dir = ""
+
+if 'nt' in os.name:
+  data_dir = "E:\\Documents\\Dropbox\\ENPH 479\\rubidium"
+else:
+  data_dir = os.environ['DATA479']
+
+
 root_dir = os.getcwd()
 
 # peak distances calculated using Dan Steck Rb 87, 85 data
@@ -56,6 +64,7 @@ source_dir_temps = data_dir + OSDirAppend('temperature_2')
 write_params_85 = root_dir + OSDirAppend('85params.csv')
 write_params_87 = root_dir + OSDirAppend('87params.csv')
 write_params_temps = root_dir + OSDirAppend('tempparams.csv')
+write_params_lock = root_dir + OSDirAppend('lockparams.csv')
 
 #
 # for plotting, each file needs the following data:
@@ -209,9 +218,9 @@ def FindRes87Power():
 
     # UNCOMMENT FOLLOWING TO VISUALLY INSPECT
 
-    # pt.plot(t_fit, v_fit, t_fit, v_sgf, linewidth=3)
-    # pt.vlines(tpeaks, min(v_fit), max(v_fit))
-    # pt.show()
+    pt.plot(t_fit, v_fit, t_fit, v_sgf, linewidth=3)
+    pt.vlines(tpeaks, min(v_fit), max(v_fit))
+    pt.show()
 
   wfile.close()
 
@@ -350,7 +359,6 @@ def round_up(num, divisor):
   else:
     return num
 
-
 def params_dict(source_file):
 
   import string
@@ -374,12 +382,42 @@ def params_dict(source_file):
 
   return pdict
 
+def slope_dict(source_file):
+
+  import string
+
+  pdict = {}
+  read_file = open(source_file, "rb")
+
+  # key, tzero, mhzps, flipped
+
+  for line in read_file:
+
+    parameters = string.split(line,",")
+
+    pdict[parameters[0]] = {}
+
+    pdict[parameters[0]]["mvps"] = float(parameters[1])
+
+    pdict[parameters[0]]["dmvps"] = float(parameters[2])
+
+    pdict[parameters[0]]["offset"] = float(parameters[3])
+
+    pdict[parameters[0]]["nmvpp"] = float(parameters[4])
+
+    pdict[parameters[0]]["in1"] = int(parameters[5])
+
+    pdict[parameters[0]]["in2"] = float(parameters[6])
+
+  return pdict
+
 def PowerFigures85():
   '''generates images in rootdirectory/figures of power data'''
 
   dump_dir = root_dir + OSDirAppend('figures')
 
   params_85 = params_dict(root_dir + OSDirAppend('85params.csv'))
+  slope_85 = slope_dict(root_dir + OSDirAppend('lockparams85.csv'))
 
   for filename in os.listdir(source_dir_85):
 
@@ -396,6 +434,16 @@ def PowerFigures85():
     pdh_absorb = file_data['v2']*1000
     pdh_error = file_data['v3']*1000
 
+    # lock point slope stuff
+
+    lock_slope = slope_85[filename]['offset'] + \
+      file_data['t1'] * slope_85[filename]['mvps']
+
+    mvpmhz = abs(slope_85[filename]['mvps']/params_85[filename]['mhzps'])
+    dmvpmhz = slope_85[filename]['dmvps']/params_85[filename]['mhzps']
+
+    nmvpp = slope_85[filename]['nmvpp']
+
     # flip if flipped
 
     if(params_85[filename]['flipped']):
@@ -403,6 +451,7 @@ def PowerFigures85():
       master_aom = master_aom[::-1]
       pdh_absorb = pdh_absorb[::-1]
       pdh_error = pdh_error[::-1]
+      lock_slope = lock_slope[::-1]
 
     fig = pt.figure()
 
@@ -421,7 +470,7 @@ def PowerFigures85():
     ax1.set_ylabel('Master (mV, Black)')
     ax1.set_ylim([-360, -180])
 
-    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#4646EA')
+    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#90b2fb')
     ax1b.set_ylabel('Mod Transfer (mV, Blue)')
     ymin, ymax = ax1b.get_ylim()
     diff = round_up(ceil((ymax-ymin)/9.0), 5)
@@ -435,6 +484,17 @@ def PowerFigures85():
     ax2.plot(mhz_scale, pdh_error, linewidth=2, color='#ff2b63')
     ax2.set_ylabel('PDH Error Signal (mV)')
     ax2.set_xlabel('Frequency from F\'=3,4 Crossover (MHz)')
+
+    ax2lims = ax2.get_ylim()
+
+    ax2.plot(mhz_scale, lock_slope, linewidth=2, color='black')
+
+    ax2.set_ylim(ax2lims)
+
+    ax2.annotate("Slope (mV/MHz): %.4f +/- %.4f" % (mvpmhz, dmvpmhz),\
+    xy=(0.55, 0.9), xycoords='axes fraction', fontsize=10)
+    ax2.annotate("Noise @ Lock (mVpp): %.4f" % (nmvpp),\
+    xy=(0.55, 0.8), xycoords='axes fraction', fontsize=10)
 
     ax1.grid(True, which='both')
     ax1.set_xticklabels([])
@@ -503,7 +563,7 @@ def PowerFigures87():
     ax1.set_ylabel('Master (mV, Black)')
     ax1.set_ylim([-360, -180])
 
-    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#4646EA')
+    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#90b2fb')
     ax1b.set_ylabel('Mod Transfer (mV, Blue)')
     ymin, ymax = ax1b.get_ylim()
     diff = round_up(ceil((ymax-ymin)/9.0), 5)
@@ -575,7 +635,7 @@ def TempFigures():
     ax1.set_ylabel('Master (mV, Black)')
 
     ax1b = ax1.twinx()
-    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#4646EA')
+    ax1b.plot(mhz_scale, pdh_absorb, linewidth=2, color='#90b2fb')
     ax1b.set_ylabel('Mod Transfer (mV, Blue)')
 
     ax1.get_xaxis().set_visible(False)
@@ -595,6 +655,151 @@ def TempFigures():
     pt.close(fig)
 
   return
+
+
+def LockPointSlopeNoise():
+  'fairly interactive. Will store slope data in mV/s, rely on peak finder \
+  to convert to mV/MHz. Will store noise value in mV'
+
+  def LineFit(axislimits, time, error, givenguess=[1.0, 1.0]):
+
+    def func(x, a, b):
+      return a*x + b
+
+    import scipy.optimize as opt
+
+    index1 = 0
+    index2 = 0
+
+    for i in xrange(0, len(time)):
+
+      if (not index1):
+        if (time[i] > axislimits[0]):
+          index1 = i
+
+      if (not index2):
+        if (time[i] > axislimits[1]):
+          index2 = i
+          break
+
+    print "Index Bounds: ", index1, index2
+
+    fittime = time[index1:index2]
+
+    fiterror = error[index1:index2]
+
+    guess = givenguess
+
+    #fits = np.polyfit(fittime, fiterror, 1)
+    fits, fcov =  opt.curve_fit(func, fittime, fiterror, guess)
+
+    print "Fitting Result:"
+    print fits[0]
+
+    mvps = fits[0]
+    dmvps = np.sqrt(fcov[0,0])
+    offset = fits[1]
+
+    flatline = fiterror - fittime*mvps # removes slope, flat line w/ noise
+    mvppn = (max(flatline) - min(flatline)) # vpp, not vrms
+
+    fitline = offset + mvps * fittime
+
+    return fittime, fitline, mvps, dmvps, offset, mvppn, index1, index2, fiterror
+
+  wfile = open(write_params_lock, 'wb')
+
+  pt.ion()
+  fig = pt.figure()
+
+  dump_dir = root_dir
+
+  for directory in [source_dir_87]:
+
+    for filename in os.listdir(directory):
+
+      fig.clf()
+
+      file_path = directory + OSDirAppend(filename)
+      file_data = np.genfromtxt(file_path, dtype=float, delimiter=',', \
+        names=['t1', 'v1', 't2', 'v2', 't3', 'v3'])
+
+      # in seconds
+
+      sec_scale = file_data['t1']
+
+      # in mV
+
+      master_aom = file_data['v1']*1000
+      pdh_absorb = file_data['v2']*1000
+      pdh_error = file_data['v3']*1000
+
+      ax1 = fig.add_subplot(111)
+      ax1.plot(sec_scale, pdh_absorb, linewidth=2, color='#324259')
+      ax1.set_ylabel('Mod Transfer (mV, Blue)')
+
+      ax1b = ax1.twinx()
+      ax1b.plot(sec_scale, pdh_error, linewidth=2, color='#ff2b63')
+      ax1b.set_ylabel('PDH Error Signal (mV)')
+
+      ax1.set_xlabel('Time(s)')
+
+      fig.show()
+
+      raw_input("Reposition Error Signal Around Lock Point,"+ \
+        "then press any key to continue...")
+
+      ax1x = ax1.get_xlim()
+
+      print "Time bounds: ", ax1x
+
+      fit_time, fit_line, mvps, dmvps, offset, vppn, in1, in2, fiterror = \
+        LineFit(ax1x, sec_scale, pdh_error)
+
+      ax1b.plot(fit_time, fit_line, linewidth=2, color='black')
+      ax1b.plot(fit_time, fiterror, linewidth=2, color='green')
+
+      print "MVPS, MVPPN: ", mvps, dmvps, vppn
+
+      prompt = raw_input("good? (y/n/c): ")
+
+      while(prompt != "y" and prompt != "c"):
+        raw_input("Reposition Error Signal Around Lock Point,"+ \
+          "then press any key to continue...")
+
+        suggestion = raw_input("Specify guessses (slope/offset)?: ")
+
+        guesses = [1.0, 1.0]
+
+        if suggestion != "":
+          guesses = np.array(suggestion.split(",")).astype(float)
+
+        ax1x = ax1.get_xlim()
+
+        print "Time bounds: ", ax1x
+
+        fit_time, fit_line, mvps, dmvps, offset, vppn, in1, in2, fiterror = \
+          LineFit(ax1x, sec_scale, pdh_error, guesses)
+
+        ax1b.plot(fit_time, fit_line, linewidth=2, color='black')
+        ax1b.plot(fit_time, fiterror, linewidth=2, color='green')
+
+        print "MVPS, MVPPN: ", mvps, vppn
+
+        prompt = raw_input("good? (y/n): ")
+
+      if prompt == "c":
+        return
+
+      wfile.write(",".join([filename, str(mvps), str(dmvps), str(offset), str(vppn), \
+        str(in1), str(in2)])+"\n")
+
+    pt.close(fig)
+
+    wfile.close()
+
+    return
+
 
 #
 # In Shell run:
